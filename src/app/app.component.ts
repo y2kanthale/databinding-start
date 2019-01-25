3ae47b889b48b7f03759ca0058af8f46de31477e
yogesh <yogdeep.k@gmail.com>
2019-01-25 16:24:24 +0530

    Learn Hooks OnInit DoCheck AfterContentInit ViewInit Checked OnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type:'server', name:'Test Server', content:'Just a test server'}];
+
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+  onChangeFirst(){
+    this.serverElements[0].name = "Changed!";
+  }
+  onDestroyFirst(){
+    this.serverElements.splice(0, 1);
+  }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,72 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  OnChanges, 
+  SimpleChanges, 
+  DoCheck, 
+  AfterContentInit, 
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild} 
+  from '@angular/core';
+import { headersToString } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
-  constructor() { }
+  constructor() { 
+    console.log( "Constructor Called" );
+  }
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges Called');
+    console.log(changes);
+  }
 
   ngOnInit() {
+    console.log( "ngOnit Called" );
+    console.log('Header Text is' + this.header.nativeElement.textContent);
+    console.log('Paragraph Text is' + this.paragraph.nativeElement.textContent);
+  }
+  ngDoCheck() {
+    console.log( "ngDoCheck Called" );
+  }
+  ngAfterContentInit() {
+    console.log( "ngAfterContentInit Called" );
+    console.log('Paragraph Text is' + this.paragraph.nativeElement.textContent);
+  }
+  ngAfterContentChecked() {
+    console.log( "ngAfterContentChecked Called" );
+  }
+  ngAfterViewInit() {
+    console.log( "ngAfterViewInit Called" );
+    console.log('Header Text is ' + this.header.nativeElement.textContent);
+  }
+  ngAfterViewChecked() {
+    console.log( "ngAfterViewChecked Called" );
+  }
+  ngOnDestroy() {
+    console.log( "ngOnDestroy Called");
   }
 
 }
